5816464e2583f7189e9cfca23744567f2c66f56b
Ashley M Mader <ashley.mader12@gmail.com>
2020-03-16 17:07:56 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,14 +1,13 @@
 class Post 
     attr_accessor :title
     attr_reader :author
-  
+    belongs_to :author
   def initialize
     @title = title
   end
   
-  def author
+  def author=(author)
     @author = author 
     author.add_title(self) unless author.title.include?(self)
-     belongs_to :author
   end
 end
