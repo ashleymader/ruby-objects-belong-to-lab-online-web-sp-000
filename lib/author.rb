39a76c490ada597248389f90cfa7221c933a91b0
Ashley M Mader <ashley.mader12@gmail.com>
2020-03-16 17:18:27 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,14 +1,8 @@
 class Author 
-  attr_accessor :title, :name
-  attr_reader :author
+  attr_accessor :title, :name, :author
   
   def initialize
     @name = name
     @titles = []
   end
-  
-  def add_title(title)
-    @titles << title
-    title.author = self if title.author != self 
-  end 
 end
