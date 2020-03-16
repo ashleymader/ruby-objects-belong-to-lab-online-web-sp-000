0119292f381c6af0d78bd6e0b7c33d9a7f6c809f
Ashley M Mader <ashley.mader12@gmail.com>
2020-03-16 17:13:20 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,16 +1,9 @@
 class Artist 
   attr_accessor :title, :name
-  attr_reader :songs
   
   def initialize
     @name = name
     @songs = []
   end
-  
-  
-  def add_song(song)
-    @songs << song
-    song.artist = self if song.artist != self 
-  end 
   
 end
